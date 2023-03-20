bce0f81b5e53eac1df75618c6e1d874b9fe7a552
Kiril <kirill.alekseevich.hr@gmail.com>
2023-03-20 11:53:17 +0200

    start

---
+++ src/components/Chat.jsx
@@ -0,0 +1,99 @@
+import { useEffect, useState } from "react"
+import io from 'socket.io-client'
+import { useLocation } from 'react-router-dom'
+import emojis from '../images/emojis.svg'
+import EmojiPicker from 'emoji-picker-react'
+
+import styles from '../styles/Chat.module.css'
+import Messages from "./Messages"
+
+const socket = io.connect("http://localhost:5000")
+
+const Chat = () => {
+
+  const { search } = useLocation();
+  const [params, setParams] = useState({
+    room: '',
+    user: '',
+  })
+  const [state, setState] = useState([])
+  const [message, setMessage] = useState('')
+  const [isOpen, setOpen] = useState(false)
+
+  useEffect(() => {
+    const searchParams = Object.fromEntries(new URLSearchParams(search))
+    setParams(searchParams)
+    socket.emit('join', searchParams)
+  }, [search])
+
+  useEffect(() => {
+    socket.on('message', ({ data }) => {
+      setState((_state) => [..._state, data])
+    })
+  }, [])
+
+  const leftRoom = () => {
+
+  }
+
+  const handleChange = ({target: { value }}) => setMessage(value)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (!message) return
+
+    socket.emit('sendMessage', { message, params })
+
+    setMessage('')
+  }
+
+  const onEmojiClick = ({ emoji }) => setMessage(`${message} ${emoji}`)
+  return (
+    <div className={styles.wrap}>
+      <div className={styles.header}>
+        <div className={styles.title}>
+          {params.room}
+        </div>
+        <div className={styles.users}>
+          0 users in this room
+        </div>
+        <button className={styles.left} onClick={leftRoom}>
+          Left the room
+        </button>
+      </div>
+
+      <div className={styles.messages}>
+        <Messages messages={state} name={params.name}/>
+      </div>
+
+      <form className={styles.form}>
+        <div className={styles.input}>
+          <input 
+            type="text" 
+            name="message" 
+            value={message}
+            placeholder='What do you want to say ?'
+            onChange={handleChange}
+            autoComplete='off'
+            required
+          />
+        </div>
+        <div className={styles.emoji}>
+          <img src={emojis} alt='emojis' onClick={() => setOpen(!isOpen)}/>
+          {
+            isOpen && (
+              <div className={styles.emojis}>
+                <EmojiPicker onEmojiClick={onEmojiClick}/>
+              </div>
+            )
+          }
+        </div>
+        <div className={styles.button}>
+          <input type="submit" onSubmit={handleSubmit} value="Send a message"/>
+        </div>
+      </form>
+    </div>
+  )
+}
+export default Chat
